4090c07f3b32d8634160867c719d63003a4a0d95
shinkeonkim <singun11@naver.com>
2020-05-30 16:15:06 +0900

    bookbankg 핵심 기능 구현 완료

--- bookbank/models.py
+++ bookbank/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class ReadingRecord(models.Model):
+    category_list = (
+        ("krBook","국내 도서"),
+        ("otherBook","국외 도서"),
+        ("eBook","eBook"),
+    )
+
+    title = models.CharField(max_length = 200)          # 책 제목
+    author = models.CharField(max_length= 100)          # 책 저자
+    
+    publisher = models.CharField(max_length = 100)      # 독서 기록 작성자(FK로 변경 필요)
+    record_title = models.CharField(max_length = 200)   # 독서 기록 제목
+    read_at = models.DateTimeField()                    # 독서 기록 일자(사용자가 정한 날짜) 
+    category = models.CharField(max_length = 100, choices = category_list) # 책 카테고리
+    record_body = models.TextField()                    # 독서 기록 내용
+
+    created_at = models.DateTimeField()                 # 독서 기록 최초 일자
+    updated_at = models.DateTimeField()                 # 독서 기록 갱신 일자
+
+    # rep_img = models.ImageField(upload_to=date_upload_to, blank = True, default = None)
+    
+    def __str__(self):
+        return self.record_title
+
+
+    def summary(self):
+        return self.record_body[:100]
--- bookbank/views.py
+++ bookbank/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import ReadingRecord
+from django.utils import timezone
+from .form import RecordForm
 
 # Create your views here.
+
+def record_list(request):
+    records = ReadingRecord.objects.all()
+    return render(request, 'record_list.html', {'records':records})
+
+# def recoed_request(request, username):
+
+def new(request):
+    if request.method == "POST":
+        form = RecordForm(request.POST) #request.FILES) -> 이미지 넣으면 필요:
+        if form.is_valid():
+            record = form.save(commit = False)
+            record.created_at = timezone.datetime.now()
+            record.updated_at = timezone.datetime.now()
+            record.publisher = request.user.username
+            record.save()
+            return redirect('record_list')
+    else:
+        form = RecordForm()
+        return render(request, 'new.html', {'form':form})
+
+def detail(request, record_id):
+    record = get_object_or_404(ReadingRecord, pk = record_id)
+    return render(request, 'detail.html', {'record':record})
+
+
+def edit(request, record_id):
+    # user = request.user
+    form = RecordForm(instance = ReadingRecord.objects.get(pk = record_id))
+    return render(request, 'edit.html', {'form':form, 'record_id':record_id})
+
+def update(request, record_id):
+    update_record = get_object_or_404(ReadingRecord, pk = record_id)
+    update_record.title = request.POST['title']
+    
+    update_record.author = request.POST['author']
+    update_record.record_title = request.POST['record_title']
+    update_record.read_at = request.POST['read_at']
+    update_record.category = request.POST['category']
+    update_record.record_body = request.POST['record_body']
+    update_record.updated_at = timezone.datetime.now()
+    # try:
+    #     update_record.rep_img = request.FILES['rep_img']
+    # except:
+    #     pass
+    update_record.save()
+    return redirect('record_detail', update_record.id)
+
+def delete(request, record_id):
+    # user = request.user
+    delete_record = get_object_or_404(ReadingRecord, pk = record_id)
+    delete_record.delete()
+    return redirect('/')
