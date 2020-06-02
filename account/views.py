17de008d46ce786c72a898834c04b36f4171d682
shinkeonkim <singun11@naver.com>
2020-06-02 13:06:52 +0900

    계정 생성 validation & alert 추가, book read_at placeholder추가

--- account/views.py
+++ account/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 # Create your views here.
 
 def signup(request):
     if request.method == "POST":
-        if request.POST["password"]  == request.POST["confirm password"]:
-            user = User.objects.create_user(
-                username = request.POST["username"], password = request.POST["password"])
-            auth.login(request, user)
-            return redirect('record_list')
+        if request.POST["password"] == "" or request.POST["password"] == None:
+            messages.error(request,"비밀번호를 입력해주세요.")
+            return render(request, 'signup.html')
+        elif request.POST["password"]  != request.POST["confirm password"]:
+            messages.error(request, "비밀번호가 서로 다릅니다.")
+            return render(request, 'signup.html')    
+        elif len(request.POST["password"]) < 4:
+            messages.error(request, "비밀번호가 너무 짧습니다.")
+            return render(request, 'signup.html')  
+        elif request.POST["password"]  == request.POST["confirm password"]:
+            try:
+                user = User.objects.create_user(
+                    username = request.POST["username"], password = request.POST["password"])
+                auth.login(request, user)
+                return redirect('record_list')
+            except:
+                messages.error(request, "다른 사용자가 사용중인 username입니다.")
+            return render(request, 'signup.html')
         return render(request, 'signup.html')
 
     return render(request, 'signup.html')
--- bookbank/form.py
+++ bookbank/form.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import ReadingRecord
+import datetime
 
 class RecordForm(forms.ModelForm):
     class Meta:
         model = ReadingRecord
         fields = ['title', 'author', 'rep_img', 'record_title', 'read_at', 'category', 'record_body']
+    
+    read_at = forms.DateField(initial=datetime.date.today,widget=forms.DateInput(attrs={'required': 'required', }))
